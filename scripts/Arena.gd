6123d169eb9ce6f137975231e74bb88a55f0a7f8
Bruno <brunobold54@outlook.com>
2021-09-16 11:49:25 +0100

    Bullet Shooting
    Added ability for player to shoot.
    Included 0.1s timeout in-between shots.

---
+++ scripts/Arena.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _ready():
+	Global.node_creation_parent = self
+	
+func _exit_tree():
+	Global.node_creation_parent = null
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,10 @@ extends Sprite
 var speed = 150
 var velocity = Vector2()
 
+var bullet = preload("res://scenes/Bullet.tscn")
+
+var can_shoot = true
+
 func _process(delta):
 	velocity.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
@@ -10,3 +14,11 @@ func _process(delta):
 	velocity = velocity.normalized()
 	
 	global_position += speed * velocity * delta
+	
+	if Input.is_action_pressed("click") and Global.node_creation_parent != null and can_shoot:
+		Global.instance_node(bullet, global_position, Global.node_creation_parent)
+		$Reload_speed.start()
+		can_shoot = false
+
+func _on_Reload_speed_timeout():
+	can_shoot = true
