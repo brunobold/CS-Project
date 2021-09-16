451b0b98b85c21fbc0bb5c0d937d6011bd41d5da
Bruno <brunobold54@outlook.com>
2021-09-16 11:25:52 +0100

    Player Movement
    Added player movement.
    
    Removed irrelevant files.

---
+++ scripts/Player.gd
@@ -0,0 +1,12 @@
+extends Sprite
+
+var speed = 150
+var velocity = Vector2()
+
+func _process(delta):
+	velocity.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	
+	velocity = velocity.normalized()
+	
+	global_position += speed * velocity * delta
